4af43836bedc0fddd78308d84f7b5974659fbad6
bronzo97 <danilobronzo97@hotmail.it>
2022-08-10 11:23:15 +0200

    adds index.blade.php

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pizza;
 use Illuminate\Http\Request;
 
 class PizzaController extends Controller
@@ -13,7 +14,9 @@ class PizzaController extends Controller
      */
     public function index()
     {
-        //
+        $pizzas = Pizza::all();
+
+        return view('pizzas.index', compact('pizzas'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('pizzas.index');
 });
+
+Route::get("/", "PizzaController@index")->name("pizzas.index");
